f8c4e553e253c0c7e512baa1ed28b06be08ac396
Robert Grancsa <robert.grancsa2002@gmail.com>
2024-05-20 10:36:14 +0300

    Added pipeline
    Signed-off-by: Robert Grancsa <robert.grancsa2002@gmail.com>

--- Whatsapp_Bot/main.py
+++ Whatsapp_Bot/main.py
@@ -1,9 +1,14 @@
 import pywhatkit
 from datetime import datetime
 
-PHONE_NUMBER = input("Enter Phone Number: ")
-MESSAGE = input("Enter Message: ")
-HR = int(input("Enter Hour: "))     # 24 Hour Format
-MIN = int(input("Enter Minutes:"))
+# PHONE_NUMBER = input("Enter Phone Number: ")
+# MESSAGE = input("Enter Message: ")
+# HR = int(input("Enter Hour: "))     # 24 Hour Format
+# MIN = int(input("Enter Minutes:"))
+
+PHONE_NUMBER = "+40775346554"
+MESSAGE = "Ala bala portocale"
+HR = datetime.now().hour
+MIN = datetime.now().minute + 1
 
 pywhatkit.sendwhatmsg(PHONE_NUMBER, MESSAGE, HR, MIN)
